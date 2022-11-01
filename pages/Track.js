f8c56785150aafdf3233a96bf5c7bd7b14909276
sid <siddhant11428@gmail.com>
2022-11-01 19:06:36 +0530

    admin and track

---
+++ pages/Track.js
@@ -0,0 +1,12 @@
+export default function Track() {
+    return (
+
+        <div className="Track">
+        
+         <p> Department:</p>
+         <p> Post:</p>
+         <p> Round:</p>
+         <p> Meeting Time:</p>
+        </div>
+    );
+}
