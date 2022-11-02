a8a06ac488efd351931a6779fe1ab7ad7c49b824
sid <siddhant11428@gmail.com>
2022-11-02 15:25:25 +0530

    applications view

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import Admin from './Admin'
-import Track from './Track'
-import Contact from './Contact'
+import Head from "next/head";
+import Admin from "./Admin";
+import Track from "./Track";
+import Contact from "./Contact";
+import Candidates from "./posts/Candidates";
 
 export default function Home() {
-  return (
-    <Contact />
-  )
+  return <Candidates />;
 }
